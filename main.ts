6ecf40a6becd4e859b92f8b76c96ee1376176f99
tahaawan123 <tahaawan598@gmail.com>
2024-05-17 00:24:18 +0500

    name game  push

---
+++ main.ts
@@ -0,0 +1,32 @@
+// Q 3.Friend checker game:
+
+// • Prompt the user to enter a name.
+// • Use a switch statement to check if the entered name is a known friend.
+// • Output a confirmation message if the name is known, otherwise output a default 
+// response.
+import inquirer from "inquirer";
+let user_Ans=await inquirer.prompt({
+    name:"namegame",
+    type: "input",
+    message:"Please Enter Your Friend Name:"
+
+});
+switch(true){
+case user_Ans.namegame === "taha awan":
+    console.log(`${user_Ans.namegame} is your friend.`);
+      break;
+ case user_Ans.namegame === "awais awan":
+            console.log(`${user_Ans.namegame} is your friend`);
+            break;
+               
+  default:
+       console.log(`${user_Ans.namegame} is not your friend.`);
+        
+}
+
+
+
+
+
+
+
